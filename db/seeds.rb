fc3713a1696ed8271217d6f7c4deaa25fa319137
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-26 17:01:28 -0500

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+list = List.create(name: Faker::Hipster.sentence)
+
+
+10.times do 
+  list.tasks.create(
+    name: Faker::Hipster.sentence,
+    completed_at: Faker::Boolean.boolean(true_ratio: 0.2) ? Time.current : nil
+  )
+end
